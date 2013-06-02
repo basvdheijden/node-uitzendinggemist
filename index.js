bca2ad38315092673f60de16fc8a88b9632b05f0
Bas van der Heijden <bas.van.der.heijden@dop.nu>
2013-06-02 19:55:41 +0200

    Module exporting

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 var UitzendingGemist = require('./lib/uitzendinggemist');
 var Overzicht = require('./lib/overzicht');
+
+module.exports = exports = function() {
+  return {
+    UitzendingGemist: UitzendingGemist,
+    Overzicht: Overzicht
+  };
+};
