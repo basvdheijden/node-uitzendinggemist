12f3367f217d6af17dd1bfd8bf307d4c119a4f41
Bas van der Heijden <bas.van.der.heijden@dop.nu>
2013-06-02 21:01:33 +0200

    Renamed to Uitzending

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-var UitzendingGemist = require('./lib/uitzendinggemist');
+var UitzendingGemistAPI = require('./lib/uitzendinggemist');
 var Overzicht = require('./lib/overzicht');
 
 module.exports = exports = function() {
   return {
-    UitzendingGemist: UitzendingGemist,
+    Uitzending: UitzendingGemistAPI,
     Overzicht: Overzicht
   };
 };
